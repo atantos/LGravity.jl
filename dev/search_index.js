52c10b2a5c98dc53ce937779c9c0a78b955a06ce
Documenter.jl <documenter@juliadocs.github.io>
2021-07-04 07:40:40 +0000

    build based on 6d284c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LGravity","category":"page"},{"location":"#LGravity","page":"Home","title":"LGravity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LGravity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LGravity]","category":"page"}]
+}
